68d5b13d098ffc3813d4718d8236baac85b9d883
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-07-11 02:52:57 +0100

    Implemented 403 error handler and the /forbidden route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
         raise error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """GET /api/v1/forbidden
+    Return:
+        raise error 403
+    """
+    abort(403)
